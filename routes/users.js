151de510295bfb040d9c3475b1d17d82d06fe6d6
AzizBenIsmail <aziz.270700@gmail.com>
2025-04-05 12:24:13 +0100

    config

--- routes/users.js
+++ routes/users.js
@@ -2,8 +2,8 @@ var express = require('express');
 var router = express.Router();
 
 /* GET users listing. */
-router.get('/', function(req, res, next) {
-  res.send('respond with a resource');
+router.get('/hello', function(req, res, next) {
+  res.status(200).json('hello 9antra 2025');
 });
 
 module.exports = router;
